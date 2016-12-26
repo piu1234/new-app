8d5d57edc328f07d42d64b9f8823cb593d4ce911
Student <Student@apples-MacBook-Air-3.local>
2016-12-26 12:34:03 +0530

    Initial Commit

---
+++ weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weather
+//
+//  Created by Student on 26/12/16.
+//  Copyright (c) 2016 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weather
+//
+//  Created by Student on 26/12/16.
+//  Copyright (c) 2016 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
